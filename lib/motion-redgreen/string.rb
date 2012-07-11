ac51b7645ed123fdd943517b8edee533e27edcba
Francis Chong <francis@ignition.hk>
2012-07-11 17:18:40 +0800

    correct color

--- lib/motion-redgreen/string.rb
+++ lib/motion-redgreen/string.rb
@@ -1,5 +1,5 @@
 class String
-  Term::ATTRIBUTE_NAMES.each do |color|
+  Term::ANSIColor::ATTRIBUTE_NAMES.each do |color|
     define_method(color) do
       Term::ANSIColor.send(color, self)
     end
