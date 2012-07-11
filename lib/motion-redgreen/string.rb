1851399607f14e861af0d12fa218b38920968366
Francis Chong <francis@ignition.hk>
2012-07-11 16:32:36 +0800

    rename origin method missing of String and use it

--- lib/motion-redgreen/string.rb
+++ lib/motion-redgreen/string.rb
@@ -1,5 +1,6 @@
 class String
+  alias_method :string_original_method_missing, :method_missing
   def method_missing(sym, *args, &block)
-    Term::ANSIColor.send(sym) { self } rescue super
+    Term::ANSIColor.send(sym) { self } rescue string_original_method_missing
   end
 end
