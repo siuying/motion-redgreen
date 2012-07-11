07b809f93ff087c10e0d514ceacd150f936f6af6
Francis Chong <francis@ignition.hk>
2012-07-11 17:17:48 +0800

    use Term::ATTRIBUTE_NAMES

--- lib/motion-redgreen/string.rb
+++ lib/motion-redgreen/string.rb
@@ -1,34 +1,5 @@
 class String
-  [  
-    :clear        ,
-    :reset        , 
-    :bold         ,
-    :dark         ,
-    :italic       , 
-    :underline    ,
-    :underscore   , 
-    :blink        ,
-    :rapid_blink  , 
-    :negative     , 
-    :concealed    ,
-    :strikethrough, 
-    :black        ,
-    :red          ,
-    :green        ,
-    :yellow       ,
-    :blue         ,
-    :magenta      ,
-    :cyan         ,
-    :white        ,
-    :on_black     ,
-    :on_red       ,
-    :on_green     ,
-    :on_yellow    ,
-    :on_blue      ,
-    :on_magenta   ,
-    :on_cyan      ,
-    :on_white     
-  ].each do |color|
+  Term::ATTRIBUTE_NAMES.each do |color|
     define_method(color) do
       Term::ANSIColor.send(color, self)
     end
