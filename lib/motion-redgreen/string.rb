52cd1358f668361057cec766cc2d6b550209fd87
Francis Chong <francis@ignition.hk>
2012-07-11 16:43:22 +0800

    rescue NoMethodError

--- lib/motion-redgreen/string.rb
+++ lib/motion-redgreen/string.rb
@@ -1,6 +1,10 @@
 class String
   alias_method :string_original_method_missing, :method_missing
   def method_missing(sym, *args, &block)
-    Term::ANSIColor.send(sym) { self } rescue string_original_method_missing(sym, *args, &block)
+    begin
+      Term::ANSIColor.send(sym) { self } 
+    rescue NoMethodError
+      string_original_method_missing(sym, *args, &block)
+    end
   end
 end
